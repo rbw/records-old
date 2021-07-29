a4be289a3699f53578bb04cfa5d44bb0bf410326
Robert Wikman <rbw@vault13.org>
2021-07-29 15:07:41 +0200

    Update exceptions

--- records/exceptions.py
+++ records/exceptions.py
@@ -12,21 +12,21 @@ class RequestError(Exception):
         self.detail = detail
 
 
-class MissingRouter(BootstrappingError):
-    """Raised if a route module is missing the `router` attribute"""
-
-
 class NoSuchRecord(RequestError):
-    """Raised if the requested record doesn't exist"""
+    """Requested record doesn't exist"""
 
 
 class DatabaseInsertError(RequestError):
-    """Raised if there was an error creating a new record in the database"""
+    """Error creating a new record in the database"""
 
 
 class PayloadDecodeError(RequestError):
-    """Raised if there was an error decoding the request JSON body"""
+    """Error decoding the request JSON body"""
 
 
 class PayloadValidationError(RequestError):
-    """Raised if the payload of a POST request failed validation"""
+    """Payload of a POST request failed validation"""
+
+
+class NoDatabaseAccess(RequestError):
+    """Database access was attempted on a Service without a database attached"""
