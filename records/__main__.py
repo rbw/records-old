48d49bf787eb5495c5abdc4f0db2b636cfb9b9bf
Robert Wikman <rbw@vault13.org>
2021-08-06 18:32:32 +0200

    Enable reading log config from env

--- records/__main__.py
+++ records/__main__.py
@@ -1,8 +1,8 @@
 import uvicorn
 from os import environ as env
 
-from . import app_make
 from extras import seed
+from . import app_make
 
 # Set up config from environment
 listen_host, listen_port = env.get("RCRD_LISTEN", "127.0.0.1:5000").split(":")
@@ -10,13 +10,14 @@ pg_username = env.get("RCRD_PG_USERNAME", "records")
 pg_password = env.get("RCRD_PG_PASSWORD", "all")
 pg_address = env.get("RCRD_PG_ADDRESS", "localhost:5432")
 pg_database = env.get("RCRD_PG_DATABASE", "records")
-debug_enable = env.get("RCRD_DEBUG", "1") == "1"
+log_config = env.get("RCRD_LOG_CONFIG", "logging.conf")
+log_debug = env.get("RCRD_LOG_DEBUG", "1") == "1"
 
 
 def main():
     return app_make(
         db_url=f"postgresql+asyncpg://{pg_username}:{pg_password}@{pg_address}/{pg_database}",
-        debug=debug_enable,
+        debug=log_debug,
         db_seed=seed
     )
 
@@ -25,8 +26,8 @@ if __name__ == "__main__":
     uvicorn.run(
         f"{__name__}:main",
         factory=True,
-        log_config="logging.conf",
+        log_config=log_config,
         host=listen_host,
         port=int(listen_port),
-        log_level="debug" if debug_enable else "info",
+        log_level="debug" if log_debug else "info",
     )
